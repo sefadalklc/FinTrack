fe57b4feff6164b7b974d3391a272f4dd6096bb1
sefadalklc <sefadalk11@gmail.com>
2023-04-15 22:34:07 +0300

    add fetcher helper and useCurrencies hook

--- components/FintrackTable/CurrencyTable/index.jsx
+++ components/FintrackTable/CurrencyTable/index.jsx
@@ -1,15 +1,28 @@
 import FintrackTable from "@/components/FintrackTable";
-import { Card, Group, Menu, SimpleGrid, Text } from "@mantine/core";
+import useCurrencies from "@/hooks/useCurrencies";
+import { createStyles } from "@mantine/core";
 import moment from "moment";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+
+
+const useStyles = createStyles((theme) => ({
+    fetchDate: {
+        fontSize: '12px',
+        color: theme.colors.gray[5],
+        textAlign: 'right'
+    },
+}));
 
 const CurrencyTable = () => {
 
-    const [date, setDate] = useState();
+    const { classes } = useStyles();
+
+    const [fetchDate, setFetchDate] = useState();
+    const { data } = useCurrencies(JSON.stringify(["BTCUSDT", "ETHUSDT", "XRPUSDT", "AVAXUSDT", "SOLUSDT", "DYDXUSDT"]));
 
     useEffect(() => {
-        setDate(moment().format('MMMM Do YYYY, h:mm:ss a'))
-    }, [])
+        setFetchDate(moment().format('MMMM Do YYYY, h:mm:ss a'))
+    }, [data])
 
     const thead = (
         <tr>
@@ -21,32 +34,7 @@ const CurrencyTable = () => {
         </tr>
     );
 
-
-    const elements = [
-        {
-            "symbol": "ETHUSDT",
-            "openPrice": 2079.19000000,
-            "highPrice": 2121.47000000,
-            "volume": 366458.80510000,
-            "lowPrice": 2071.13000000,
-        },
-        {
-            "symbol": "BTCUSDT",
-            "openPrice": 30275.00000000,
-            "highPrice": 30595.60000000,
-            "volume": 29547.43968000,
-            "lowPrice": 30200.76000000,
-        },
-        {
-            "symbol": "XRPUSDT",
-            "openPrice": "0.52020000",
-            "highPrice": "0.52780000",
-            "lowPrice": "0.51600000",
-            "volume": "175324802.00000000",
-        }
-    ]
-
-    const rows = elements.map((element) => (
+    const rows = data?.map((element) => (
         <tr key={element.symbol}>
             <td>{element.symbol}</td>
             <td>{Number(element.highPrice).toFixed(4)}</td>
@@ -56,17 +44,13 @@ const CurrencyTable = () => {
         </tr>
     ));
 
-    const tfoot = <tr>
-        <td>{date}</td>
-    </tr>
-
     return (
         <>
             <FintrackTable
                 thead={thead}
-                tfoot={tfoot}
                 rows={rows}
             />
+            <p className={classes.fetchDate}>Date: {fetchDate}</p>
         </>
     )
 }
---
+++ hooks/useCurrencies.ts
@@ -0,0 +1,16 @@
+import useSWR from 'swr'
+import fetcher from "@/libs/fetcher"
+import moment from "moment";
+
+const useCurrencies = (currencyQuery: string) => {
+
+    const { data, error, isLoading } = useSWR(`http://api.binance.com/api/v3/ticker/24hr?symbols=${currencyQuery}`, fetcher)
+
+    return {
+        data,
+        error,
+        isLoading,
+    }
+}
+
+export default useCurrencies;
