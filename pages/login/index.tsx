ad839d1f68dcf49c69b7aad733238300d9aeebad
sefadalklc <sefadalk11@gmail.com>
2023-04-16 16:44:28 +0300

    add yup and formik, implemented login validations

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,17 +3,19 @@ import {
   createStyles,
   TextInput,
   PasswordInput,
-  Checkbox,
   Button,
   Title,
   Text,
   rem,
+  Alert,
 } from '@mantine/core';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useCallback, useState } from 'react';
 import { signIn } from "next-auth/react";
 import AuthLayout from '@/layouts/authLayout';
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { NextPage } from 'next';
+import { IconAlertCircle } from '@tabler/icons-react';
 
 const useStyles = createStyles((theme) => ({
   wrapper: {
@@ -23,8 +25,7 @@ const useStyles = createStyles((theme) => ({
   },
 
   form: {
-    borderRight: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
-      }`,
+    borderRight: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]}`,
     minHeight: rem(900),
     maxWidth: rem(450),
     paddingTop: rem(80),
@@ -39,7 +40,6 @@ const useStyles = createStyles((theme) => ({
     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
   },
 
-
   link: {
     textDecoration: 'none',
     color: theme.colors.blue[8],
@@ -48,58 +48,74 @@ const useStyles = createStyles((theme) => ({
 
 }));
 
+const loginSchema = Yup.object().shape({
+  email: Yup.string().required("Email alanı gereklidir.").email("Geçerli bir e-posta formatı olmalıdır."),
+  password: Yup.string().required("Şifre alanı gereklidir.").min(6, "En az 6 karakterden oluşmalıdır."),
+});
 
-const Login = () => {
+const Login: NextPage = () => {
   const { classes } = useStyles();
-  const router = useRouter();
 
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
 
-  const login = useCallback(async () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
 
-    await signIn('credentials', {
-      email,
-      password,
-    });
+    validationSchema: loginSchema,
 
-  }, [email, password])
+    onSubmit: async ({ email, password }) => {
+      await signIn('credentials', {
+        email,
+        password,
+      });
+    },
+  });
+
+  const { errors, touched, values, handleChange, handleSubmit } = formik;
 
   return (
     <div className={classes.wrapper}>
-      <Paper className={classes.form} radius={0} p={30}>
-        <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
-          FinTrack
-        </Title>
+      <form method='POST' onSubmit={handleSubmit}>
+        <Paper className={classes.form} radius={0} p={30}>
+          <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
+            FinTrack
+          </Title>
 
-        <TextInput
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          label="Email adresiniz"
-          placeholder="Email adresinizi giriniz"
-          size="lg"
-        />
-        <PasswordInput
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          label="Şifre"
-          placeholder="Şifrenizi giriniz"
-          mt="md"
-          size="lg"
-        />
-        <Checkbox label="Beni hatırla" mt="xl" size="md" />
-        <Button fullWidth mt="xl" size="md" onClick={login}>
-          Giriş Yap
-        </Button>
+          <TextInput
+            value={values.email}
+            onChange={handleChange}
+            placeholder="Email adresinizi giriniz"
+            size="lg"
+            id="email"
+          />
+          {errors.email && touched.email && <Alert mt={10} icon={<IconAlertCircle size="2rem" />} color="red" radius="md" p="xs" variant="outline">{errors.email}</Alert>}
 
-        <Text ta="center" mt="md">
-          Hesabınız yok mu? {' '}
-          <Link href="/register" className={classes.link}>
-            Kayıt Ol
-          </Link>
-        </Text>
-      </Paper>
-    </div>
+          <PasswordInput
+            value={values.password}
+            onChange={handleChange}
+            label="Şifreniz"
+            placeholder="Şifrenizi giriniz"
+            mt="md"
+            size="lg"
+            id="password"
+          />
+          {errors.password && touched.password && <Alert mt={10} icon={<IconAlertCircle size="2rem" />} color="red" radius="md" p="xs" variant="outline">{errors.password}</Alert>}
+
+          <Button fullWidth mt="xl" size="md" type='submit'>
+            Giriş Yap
+          </Button>
+
+          <Text ta="center" mt="md">
+            Hesabınız yok mu? {' '}
+            <Link href="/register" className={classes.link}>
+              Kayıt Ol
+            </Link>
+          </Text>
+        </Paper>
+      </form>
+    </div >
   );
 }
 
